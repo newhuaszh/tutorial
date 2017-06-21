914cc4b9d52e13800a5927755ecb3fa6abd2704f
szh <szhhzs@sina.com>
2017-06-21 17:34:25 +0800

    Initial commit

---
+++ quickstart/serializers.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+__author__ = 'szh'
+__date__ = '2017/6/21 0021 17:29'
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
